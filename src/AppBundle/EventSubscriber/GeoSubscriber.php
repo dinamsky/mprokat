7fd2c0bfa221e450c9561be6ed6c8ba017c46112
Timur Malyshev <wqs-info@mail.ru>
2017-09-09 20:33:07 +0500

    fix geo 3

--- src/AppBundle/EventSubscriber/GeoSubscriber.php
+++ src/AppBundle/EventSubscriber/GeoSubscriber.php
@@ -15,31 +15,31 @@ class GeoSubscriber implements EventSubscriberInterface
     public function onKernelController(FilterControllerEvent $event)
     {
 
-        $is_bot = preg_match(
-            "~(Google|Yahoo|Rambler|Bot|Yandex|Spider|Snoopy|Crawler|Finder|Mail|curl)~i",
-            $_SERVER['HTTP_USER_AGENT']
-        );
-
-        if(!$is_bot){
-
-            $ip = $event->getRequest()->getClientIp();
-            if ($ip == 'unknown') {
-                $ip = $_SERVER['REMOTE_ADDR'];
-            }
-            if ($ip == '127.0.0.1') $ip = '94.41.250.18';
-
-            $sessId = $event->getRequest()->getSession()->getId();
-
-            if ($event->getRequest()->getSession()->has('ip')) {
-                $sessIP = $event->getRequest()->getSession()->get('ip');
-            } else {
-                $event->getRequest()->getSession()->set('ip', $ip);
-                $event->getRequest()->getSession()->set('sessId', $sessId);
-                $geo = json_decode(file_get_contents('http://api.sypexgeo.net/json/'.$ip), true);
-                $event->getRequest()->getSession()->set('geo', $geo);
-                $event->getRequest()->getSession()->getFlashBag()->add('notice', 'Your city:'.$geo['city']['name_ru']);
-            }
-        }
+//        $is_bot = preg_match(
+//            "~(Google|Yahoo|Rambler|Bot|Yandex|Spider|Snoopy|Crawler|Finder|Mail|curl)~i",
+//            $_SERVER['HTTP_USER_AGENT']
+//        );
+//
+//        if(!$is_bot){
+//
+//            $ip = $event->getRequest()->getClientIp();
+//            if ($ip == 'unknown') {
+//                $ip = $_SERVER['REMOTE_ADDR'];
+//            }
+//            if ($ip == '127.0.0.1') $ip = '94.41.250.18';
+//
+//            $sessId = $event->getRequest()->getSession()->getId();
+//
+//            if ($event->getRequest()->getSession()->has('ip')) {
+//                $sessIP = $event->getRequest()->getSession()->get('ip');
+//            } else {
+//                $event->getRequest()->getSession()->set('ip', $ip);
+//                $event->getRequest()->getSession()->set('sessId', $sessId);
+//                $geo = json_decode(file_get_contents('http://api.sypexgeo.net/json/'.$ip), true);
+//                $event->getRequest()->getSession()->set('geo', $geo);
+//                $event->getRequest()->getSession()->getFlashBag()->add('notice', 'Your city:'.$geo['city']['name_ru']);
+//            }
+//        }
 
         //dump($event->getRequest());
     }
