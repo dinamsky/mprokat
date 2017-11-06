2baa586455f80b564e045752ba297a872105a888
Timur Malyshev <wqs-info@mail.ru>
2017-11-06 17:46:18 +0500

    abuse test 3

--- web/assets/js/recaptcha_settings.js
+++ web/assets/js/recaptcha_settings.js
@@ -1,6 +1,4 @@
-var verifyCallback = function(response) {
-        alert(response);
-      };
+
 
 var widgetId1;
 var widgetId2;
@@ -8,12 +6,12 @@ var onloadCallback = function() {
 // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
 // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
     widgetId1 = grecaptcha.render('recap1', {
-        'sitekey' : '6LcGCzUUAAAAADpeaCQhkXWZqdhnB6_ZEGRm7Z2m',
-        'theme' : 'light',
-        'callback' : verifyCallback
+        'sitekey' : '6LcGCzUUAAAAADpeaCQhkXWZqdhnB6_ZEGRm7Z2m'
+        'theme' : 'light'
+
     });
     widgetId2 = grecaptcha.render('recap2', {
-        'sitekey' : '6LcGCzUUAAAAADpeaCQhkXWZqdhnB6_ZEGRm7Z2m',
-        'callback' : verifyCallback
+        'sitekey' : '6LcGCzUUAAAAADpeaCQhkXWZqdhnB6_ZEGRm7Z2m'
+
     });
 };
