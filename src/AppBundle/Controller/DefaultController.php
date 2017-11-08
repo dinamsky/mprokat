237d1b4a7d49d1d4016c6e50667a7a1ee15ccb26
Timur Malyshev <wqs-info@mail.ru>
2017-11-08 12:57:59 +0500

    plane subfields fix 1 fix wp_stub

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -70,45 +70,18 @@ class DefaultController extends Controller
      */
     public function wpStubAction(MenuGeneralType $mgt, MenuCity $mc, EntityManagerInterface $em, MenuMarkModel $mm)
     {
-        $city = $this->getDoctrine()
-            ->getRepository(City::class)
-            ->find(77);
+        $city = $this->get('session')->get('city');
 
-        $general = $this->getDoctrine()
-            ->getRepository(GeneralType::class)
-            ->find(2);
+        $in_city = $city->getUrl();
 
         $query = $em->createQuery('SELECT g,c FROM AppBundle:GeneralType g LEFT JOIN g.cards c');
         $generalTypes = $query->getResult();
 
         return $this->render('common/wp_stub.html.twig', [
-            'generalTopLevel' => $mgt->getTopLevel(),
-            'cards' => '',
-            'custom_fields' => '',
-            'general_type' => null,
             'city' => $city,
-            'mark_model' => array(),
-            'mark_groups' => $mm->getGroups(),
-
-            'countries' => $mc->getCountry(),
-            'countryCode' => $city->getCountry(),
-            'regionId' => $city->getParentId(),
-            'regions' => $mc->getRegion($city->getCountry()),
-            'cities' => $mc->getCities($city->getParentId()),
             'cityId' => $city->getId(),
-
-            'gtid' => 2,
-            'pgtid' => 1,
-            'generalSecondLevel' => $mgt->getSecondLevel(1),
-
-            'marks' => [],
-            'models' => [],
-            'mark' => ['id'=>0,'groupname'=>'','header'=>false],
-            'model' => ['id'=>0, 'header'=>false],
-
-            'general' => $general,
-
-            'generalTypes' => $generalTypes
+            'generalTypes' => $generalTypes,
+            'in_city' => $in_city
         ]);
     }
 
