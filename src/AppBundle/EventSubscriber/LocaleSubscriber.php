4c3e565082d5019021540525abaf1f2b04de5c9c
Timur Malyshev <wqs-info@mail.ru>
2017-11-11 14:00:41 +0500

    translate card

--- src/AppBundle/EventSubscriber/LocaleSubscriber.php
+++ src/AppBundle/EventSubscriber/LocaleSubscriber.php
@@ -6,7 +6,7 @@ namespace AppBundle\EventSubscriber;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Translation\Loader\PoFileLoader;
 use Symfony\Component\Translation\Translator;
 use Symfony\Component\Translation\MessageSelector;
@@ -14,15 +14,22 @@ use Symfony\Component\Translation\MessageSelector;
 class LocaleSubscriber implements EventSubscriberInterface
 {
     private $defaultLocale;
+    private $env;
 
-    public function __construct($defaultLocale = 'en')
+    public function __construct($defaultLocale = 'en', KernelInterface $kernel)
     {
         $this->defaultLocale = $defaultLocale;
+        $this->env = $kernel->getEnvironment();
     }
 
     public function onKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
+        $ip = $event->getRequest()->getClientIp();
+
+        if($this->env == 'dev' and $ip == '127.0.0.1'){
+            $_SERVER['LANG'] = 'en';
+        }
 
         if(!isset($_SERVER['LANG'])) $_SERVER['LANG'] = 'ru';
 
