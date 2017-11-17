c79643e34558da5a46801d90c5bde63a5e4f03e6
Timur Malyshev <wqs-info@mail.ru>
2017-11-17 18:08:20 +0500

    des pop_search 2

--- web/assets/js/search_popup.js
+++ web/assets/js/search_popup.js
@@ -1,7 +1,70 @@
 $( document ).ready(function() {
 
-    $('.search_popup_trigger').on('click', function () {
-        $('#search_popup').toggleClass('uk-hidden');
+    var my_city_autoComplete_search_popup = new autoComplete({
+        selector: 'input[name="popup_search"]',
+        source: function(term, response){
+            $.ajax({
+                url: '/ajax/getCityByInput',
+                type: 'POST',
+                dataType: 'json',
+                data: {q: term},
+                success: function(json){
+
+                    response(json);
+                }
+            });
+        },
+        renderItem: function (item, search){
+            var res = item.split('|');
+            item = res[0];
+            var id = res[1];
+            search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+            var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+            return '<div class="autocomplete-suggestion city_block" data-url="' + res[2] + '" data-header="' + item + '" data-id="' + id + '" data-val = "' + item + '" >' + item.replace(re, "<b>$1</b>") + '</div>';
+        },
+        onSelect: function(e, term, item){
+            $('.city_block[data-id="'+item.getAttribute('data-id')+'"]').click();
+            $('input[name="input_city"]').val('');
+        }
     });
+
+    $('.search_popup_trigger').on('click', function (e) {
+        e.stopPropagation();
+        $('#search_popup').removeClass('uk-hidden');
+    });
+
+    $('body').on('click', function () {
+        if(!$('#search_popup').hasClass('uk-hidden')) $('#search_popup').addClass('uk-hidden');
+    });
+
+    $('#search_popup').on('click', function (e) {
+        e.stopPropagation();
+    });
+
+    $('.top_pop_carousel').owlCarousel({
+        'nav': true,
+        'margin' : 5,
+        'slideBy' : 4,
+        'navText': ['<i uk-icon="icon:chevron-left"></i>', '<i uk-icon="icon:chevron-right"></i>'],
+        // 'autoHeight': true,
+        //'dots': dots,
+        'items':4,
+        responsive:{
+            0:{
+
+            },
+            600:{
+
+            },
+            1000:{
+
+            },
+            2000:{
+
+            }
+        }
+
+    });
+
 
 });
