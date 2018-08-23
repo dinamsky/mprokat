852414a57e6b59a52c565ebfaf9e2f6271b1e566
Timur Malyshev <wqs-info@mail.ru>
2018-08-23 19:21:32 +0500

    fix hidden recaptcha

--- web/assets/js/recaptcha_settings.js
+++ web/assets/js/recaptcha_settings.js
@@ -12,12 +12,12 @@ var widgetId3;
 var onloadCallback = function() {
 // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
 // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
-    widgetId1 = grecaptcha.render('recap1', {
-        'sitekey' : '6LcGCzUUAAAAADpeaCQhkXWZqdhnB6_ZEGRm7Z2m',
-        'theme' : 'light',
-        'callback' : verifyCallback
-
-    });
+//     widgetId1 = grecaptcha.render('recap1', {
+//         'sitekey' : '6LcGCzUUAAAAADpeaCQhkXWZqdhnB6_ZEGRm7Z2m',
+//         'theme' : 'light',
+//         'callback' : verifyCallback
+//
+//     });
     widgetId2 = grecaptcha.render('recap2', {
         'sitekey' : '6LcGCzUUAAAAADpeaCQhkXWZqdhnB6_ZEGRm7Z2m',
 'callback' : verifyCallback
