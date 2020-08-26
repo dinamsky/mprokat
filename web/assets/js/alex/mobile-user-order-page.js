0e1eb96edfa502ddcbf7017b1ffc312837d533ca
Igor Dinamsky <dinamsky@gmail.com>
2020-08-26 09:47:24 +0200

    last ch

---
+++ web/assets/js/alex/mobile-user-order-page.js
@@ -0,0 +1,32 @@
+const mobileUserOrderPage = (function($) {
+
+    let ui = {
+        $messageInputField: $('.js-message-input-field')
+    };
+
+    function init() {
+        _bindHandlers();
+    }
+
+    function _bindHandlers() {
+        ui.$messageInputField.on('input', _autoResizeField);
+    }
+
+    function _autoResizeField(e) {
+
+        if(!$(this).attr('rows') < 3) {
+            this.style = '';
+            return;
+        }
+
+        this.style.height = '1px';
+        this.style.height = (this.scrollHeight + 6) + 'px';
+    }
+
+    return {
+        init: init
+    }
+
+})(jQuery);
+
+jQuery(document).ready(mobileUserOrderPage.init);
